adbd0d669b66b77855a043a3461c168e81913088
dimasrangga <derangga1011@gmail.com>
2025-04-08 16:17:32 +0700

    fix microlink bg when dark mode

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -9,10 +9,15 @@ const Blog = () => {
       initial={{ y: 20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ ease: "easeInOut", duration: 0.75 }}
-      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
+      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
     >
       {blog.map((e, idx) => (
-        <Microlink url={e} key={idx + 1} className="rounded-xl" size="large" />
+        <Microlink
+          url={e}
+          key={idx + 1}
+          className="rounded-xl hover:scale-105 transform transition-transform"
+          size="large"
+        />
       ))}
     </motion.div>
   );
