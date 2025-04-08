aa7ca28afcd60953ba47c732dea150adc5bc89c8
dimasrangga <derangga1011@gmail.com>
2025-04-08 15:39:13 +0700

    enable project item can be clicked

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -1,14 +1,20 @@
 "use client";
 import { blog } from "@/constants/content";
 import Microlink from "@microlink/react";
+import { motion } from "framer-motion";
 
 const Blog = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+    <motion.div
+      initial={{ y: 20, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ ease: "easeInOut", duration: 0.75 }}
+      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
+    >
       {blog.map((e, idx) => (
         <Microlink url={e} key={idx + 1} className="rounded-xl" size="large" />
       ))}
-    </div>
+    </motion.div>
   );
 };
 
